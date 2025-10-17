e508f58fb200e8f34d789938e209bd3da29f8927
Ramon Moncao <ramonmoncao@hotmail.com>
2025-10-17 14:37:25 -0300

    Correções login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,20 @@
-"use client"
+"use client";
 
 import { useEffect, useState } from "react";
 // import { createClient } from "@/utils/supabase/client";
 import { useRouter } from "next/navigation";
 import Header from "@/components/header";
+import Image from "next/image";
+import Button from "@/components/button";
 
 export default function Login() {
-
   // const supabase = createClient()
-  const router = useRouter()
-  const [checkingSession, setCheckingSession] = useState(true)
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [isLoading, setIsLoading] = useState(false)
-  const [errorMessage, setErrorMessage] = useState("")
+  const router = useRouter();
+  const [checkingSession, setCheckingSession] = useState(true);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
     const checkSession = async () => {
@@ -23,14 +24,14 @@ export default function Login() {
       // } else {
       //   setCheckingSession(false)
       // }
-    }
-    checkSession()
-  }, [])
+    };
+    checkSession();
+  }, []);
 
   const login = async (e: React.FormEvent) => {
-    e.preventDefault()
-    setIsLoading(true)
-    setErrorMessage("")
+    e.preventDefault();
+    setIsLoading(true);
+    setErrorMessage("");
 
     // try {
     //   const { data, error } = await supabase.auth.signInWithPassword({ email, password })
@@ -46,7 +47,7 @@ export default function Login() {
     //   setErrorMessage("Erro inesperado. Tente novamente.")
     //   setIsLoading(false)
     // }
-  }
+  };
 
   // Sem essa verificação, ao acessar a raiz LOGADO, ele
   // por um segundo ainda aparece a tela de login antes de redirecionar para dashboard
@@ -56,53 +57,52 @@ export default function Login() {
   // }
 
   return (
-    <div className="flex flex-col h-screen">
-      <div style={{ backgroundColor: "var(--background)" }}>
-        <Header />
+    <div className="flex h-screen flex-col md:flex-row">
+      <div className="relative hidden md:block md:w-1/2 h-screen">
+        <Image
+          src="/bg-login.png"
+          alt="background"
+          fill
+          className="object-cover"
+        />
       </div>
-      <div className="flex h-screen flex-col md:flex-row md:items-stretch md:justify-end">
-        <div className="w-full md:w-auto flex items-start p-6 md:justify-end h-full"
-          style={{ backgroundColor: "var(--background)" }}>
-          <div className="w-full max-w-md p-8" >
-            <h2 className="text-2xl font-extrabold text-center mb-30"
-              style={{ color: "var(--color-primary-1)" }}>Acesse sua conta</h2>
-            {/* <p className="text-gray-500 mb-6">Insira as informações que você usou ao se registrar.</p> */}
-            <form onSubmit={login}>
-              <h2 className="flex font-extrabold"
-                style={{ color: "var(--color-text-1)" }}>Usuário</h2>
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                className="w-full p-3 mb-15 shadow-lg rounded-md focus:ring-2 focus:ring-blue-500"
-                style={{ backgroundColor: "var(--color-box-1)" }}
-                required
-              />
-              {errorMessage && (
-                <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
-              )}
-              <h2 className="flex font-extrabold"
-                style={{ color: "var(--color-text-1)" }}>Senha</h2>
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                className="w-full p-3 mb-20 shadow-lg rounded-md focus:ring-2 focus:ring-blue-500"
-                style={{ backgroundColor: "var(--color-box-1)" }}
-                required
-              />
-              <button
-                type="submit"
-                disabled={isLoading}
-                className="w-full text-white p-3 rounded-md font-extrabold hover:opacity-90 transition-all disabled:opacity-50"
-                style={{ backgroundColor: "var(--color-primary-2)" }}
-              >
-                {isLoading ? "Entrando" : "Entrar"}
-              </button>
-            </form>
-          </div>
+      <div className="flex w-full md:w-1/2  justify-center p-6">
+        <div className="w-full max-w-md p-8 rounded-2xl md:shadow-none md:bg-transparent shadow-2xl bg-[var(--color-box-1)]">
+          <h2 className="text-[var(--color-primary-1)] text-xl font-extrabold text-center mb-8 ">
+            Acesse sua conta
+          </h2>
+
+          <form onSubmit={login}>
+            <label className="flex font-extrabold mb-2 text-[var(--color-text-1)]">
+              Usuário
+            </label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full p-3 mb-4 shadow-lg rounded-md focus:ring-2 focus:ring-white bg-[var(--color-input-1)]"
+              required
+            />
+            {errorMessage && (
+              <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
+            )}
+
+            <label className="flex font-extrabold mt-4 mb-2 text-[var(--color-text-1)]">
+              Senha
+            </label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="w-full p-3 mb-20 shadow-lg rounded-md focus:ring-2 focus:ring-white bg-[var(--color-input-1)]"
+              required
+            />
+            <Button type="submit" isLoading={isLoading}>
+              Entrar
+            </Button>
+          </form>
         </div>
       </div>
     </div>
-  )
+  );
 }
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,7 +1,19 @@
-const Button = () => {
-    return(
-        <div></div>
-    )
+import React, { ButtonHTMLAttributes } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  isLoading?: boolean;
 }
 
-export default Button
+const Button: React.FC<ButtonProps> = ({ children, isLoading = false, disabled, ...props }) => {
+  return (
+    <button
+      {...props}
+      disabled={disabled || isLoading}
+      className="w-full text-white p-3 rounded-md font-extrabold hover:opacity-90 transition-all disabled:opacity-50 bg-[var(--color-primary-2)]"
+    >
+      {isLoading ? "Carregando..." : children}
+    </button>
+  );
+};
+
+export default Button;
