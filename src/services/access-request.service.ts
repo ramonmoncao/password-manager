241c637aae65c02e5bc07e32f716f9b778cf148c
Ramon Monção dos Santos <ramonmoncao@hotmail.com>
2025-11-24 21:47:17 -0300

    Feature/gerenciamento de acessos (#15)
    * Atualização de senhas automatizada
    
    * Gerenciamento de acessos

--- src/services/access-request.service.ts
+++ src/services/access-request.service.ts
@@ -4,6 +4,7 @@ import { sendEmailById } from "./send-email.service";
 export interface IAccessRequest {
   id: number;
   id_user: string;
+  user_name: string;
   id_project: number;
   reason: string;
   created_at: string;
@@ -85,3 +86,56 @@ export const createAccessRequest = async (request: Partial<IAccessRequest>) => {
   if (error) throw error;
   return data;
 };
+
+
+export const getPendingAccessRequest = async (admin_id: string): Promise<IAccessRequest[]> => {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase
+    .rpc("get_access_request_to_accept", { admin_id });
+
+  if (error) {
+    console.error("Erro ao buscar access requests via RPC:", error);
+    return [];
+  }
+
+  return data as IAccessRequest[];
+};
+
+export const acceptAccessRequest = async (id: number) => {
+  const supabase = await createClient();
+  const { data: updatedRequest, error: updateError } = await supabase
+    .from("access_request")
+    .update({
+      active: false,
+      accepted: true,
+    })
+    .eq("id", id)
+    .select()
+    .single(); 
+
+  if (updateError) throw updateError;
+  if (!updatedRequest) throw new Error("Solicitação não encontrada");
+
+  await createAccess(
+    updatedRequest.id_project_group,
+    updatedRequest.id_user,
+    updatedRequest.temporary
+  );
+
+  return updatedRequest;
+};
+
+
+export const rejectAccessRequest = async(id: number) => {
+  const supabase = await createClient();
+
+  const {data, error} = await supabase
+  .from("access_request")
+  .update({
+    active: false,
+    accepted: false
+  })
+  .eq("id", id)
+
+}
